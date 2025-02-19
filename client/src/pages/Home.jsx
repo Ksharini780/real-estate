94c3356f78ad8da52f1f56882fcc3b7980e2a9f6
Ksharini780 <111514299+Ksharini780@users.noreply.github.com>
2025-02-19 23:26:08 +0530

    connect to database

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -55,14 +55,14 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Real Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
         </div>
         <Link
           to={'/search'}
-          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+          className='text-xs sm:text-sm text-slate-800 font-bold hover:underline'
         >
           Let's get started...
         </Link>
